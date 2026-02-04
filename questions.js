79c02b25b90cd16d9270e263c3c1e114754086c8
durgeshwarpratapsingh7 <durgeshwar.pratapsingh.7@gmail.com>
2026-02-04 19:30:31 +0530

    Create questions.js

---
+++ questions.js
@@ -0,0 +1,40 @@
+const questions = {
+    biology: [
+        {
+            q: "Which organelle is called powerhouse of the cell?",
+            options: ["Nucleus", "Ribosome", "Mitochondria", "Chloroplast"],
+            answer: 2
+        },
+        {
+            q: "DNA is located in?",
+            options: ["Nucleus", "Cytoplasm", "Mitochondria", "Both A and C"],
+            answer: 3
+        }
+    ],
+
+    chemistry: [
+        {
+            q: "pH of pure water is?",
+            options: ["5", "7", "9", "11"],
+            answer: 1
+        },
+        {
+            q: "Atomic number represents?",
+            options: ["Protons", "Neutrons", "Electrons", "Mass"],
+            answer: 0
+        }
+    ],
+
+    physics: [
+        {
+            q: "SI unit of Force is?",
+            options: ["Joule", "Newton", "Pascal", "Watt"],
+            answer: 1
+        },
+        {
+            q: "Speed of light is?",
+            options: ["3×10^8 m/s", "3×10^6 m/s", "3×10^5 m/s", "3×10^3 m/s"],
+            answer: 0
+        }
+    ]
+};
